f54029d937e537fc15ad1dc788c7db8081012ca9
Shefali Agarwal <shefali.agarwal251@gmail.com>
2018-10-04 15:10:05 -0400

    Home module: added camera functionality

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,43 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  public base64Image: string;
+  constructor(public navCtrl: NavController, private camera: Camera) {
+    
   }
 
+  // openCamera(selection: any) {
+  //   const options: CameraOptions = {
+  //     quality: 100,
+  //     destinationType: this.camera.DestinationType.FILE_URI,
+  //     encodingType: this.camera.EncodingType.JPEG,
+  //     mediaType: this.camera.MediaType.PICTURE
+  //   }
+    
+  //   this.camera.getPicture(options).then((imageData) => {
+  //    // imageData is either a base64 encoded string or a file URI
+  //    // If it's base64 (DATA_URL):
+  //    let base64Image = 'data:image/jpeg;base64,' + imageData;
+  //   }, (err) => {
+  //    // Handle error
+  //   });
+  // }
+  takePicture(){
+    this.camera.getPicture({
+        destinationType: this.camera.DestinationType.DATA_URL,
+        targetWidth: 1000,
+        targetHeight: 1000
+    }).then((imageData) => {
+      // imageData is a base64 encoded string
+        this.base64Image = "data:image/jpeg;base64," + imageData;
+    }, (err) => {
+        console.log(err);
+    });
+  }
 }
